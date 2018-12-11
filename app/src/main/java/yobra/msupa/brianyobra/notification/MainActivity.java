e9a6dcef3b96b9867140f902aa3837c6316694a9
yobra89 <nonini89@gmail.com>
2018-12-11 14:49:57 +0300

    Initial commit

---
+++ app/src/main/java/yobra/msupa/brianyobra/notification/MainActivity.java
@@ -0,0 +1,120 @@
+package yobra.msupa.brianyobra.notification;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends Activity {
+    private NotificationManager mNotificationManager;
+    private int notificationID = 100;
+    private int numMessages = 0;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startBtn = (Button) findViewById(R.id.start);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                displayNotification();
+            }
+        });
+        Button cancelBtn = (Button) findViewById(R.id.cancel);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                cancelNotification();
+            }
+        });
+        Button updateBtn = (Button) findViewById(R.id.update);
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                updateNotification();
+            }
+        });
+    }
+    protected void displayNotification() {
+        Log.i("Start", "notification");
+/* Invoking the default notification service */
+        NotificationCompat.Builder
+                mBuilder =
+                new NotificationCompat.Builder(this);
+        mBuilder.setContentTitle("New Message");
+        mBuilder.setContentText("You've received new message.");
+        mBuilder.setTicker("New Message Alert!");
+        mBuilder.setSmallIcon(R.drawable.ic_launcher);
+/* Increase notification number every time a new notification
+arrives */
+        mBuilder.setNumber(++numMessages);
+/* Creates an explicit intent for an Activity in your app */
+
+        Intent resultIntent = new Intent(this, Notification.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(Notification.class);
+
+/* Adds the Intent that starts the Activity to the top of the stack
+*/
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        mNotificationManager =
+                (NotificationManager)
+                        getSystemService(Context.NOTIFICATION_SERVICE);
+/* notificationID allows you to update the notification later on.
+ */
+
+        mNotificationManager.notify(notificationID, mBuilder.build());
+    }
+    protected void cancelNotification() {
+        Log.i("Cancel", "notification");
+        mNotificationManager.cancel(notificationID);
+    }
+    protected void updateNotification() {
+        Log.i("Update", "notification");
+/* Invoking the default notification service */
+        NotificationCompat.Builder
+                mBuilder =
+                new NotificationCompat.Builder(this);
+        mBuilder.setContentTitle("Updated Message");
+        mBuilder.setContentText("You've got updated message.");
+        mBuilder.setTicker("Updated Message Alert!");
+        mBuilder.setSmallIcon(R.drawable.ic_launcher);
+/* Increase arrives notification number everytime a new notification */
+
+
+        mBuilder.setNumber(++numMessages);
+/* Creates an explicit intent for an Activity in your app */
+
+
+        Intent resultIntent = new Intent(this, Notification.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(Notification.class);
+
+/* Adds the Intent that starts the Activity to the top of the stack
+*/
+        stackBuilder.addNextIntent(resultIntent);
+
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        mNotificationManager =
+                (NotificationManager)
+                        getSystemService(Context.NOTIFICATION_SERVICE);
+/* Update the existing notification using same notification ID */
+        mNotificationManager.notify(notificationID, mBuilder.build());
+    }
+}
---
+++ app/src/main/java/yobra/msupa/brianyobra/notification/Notification.java
@@ -0,0 +1,17 @@
+package yobra.msupa.brianyobra.notification;
+
+/**
+ * Created by brianyobra on 8/27/18.
+ */
+import android.os.Bundle;
+import android.app.Activity;
+
+
+public class Notification extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
